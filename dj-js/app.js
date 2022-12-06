461c93bf70fc676378b107afae76123af5840079
michael hosler <mimic.orange@gmail.com>
2022-12-06 17:40:19 -0600

    finished

--- dj-js/app.js
+++ dj-js/app.js
@@ -1,30 +1,46 @@
 var djSquare = document.getElementById("square")
 
-function blueSquare(){
-   djSquare.style.backgroundColor = "blue"
-   console.log ("food")
-}
+    function blueSquare(){
+    djSquare.style.backgroundColor = "blue"
+    }
 
-function whiteSquare(){
-    djSquare.style.backgroundColor = "white"
-}
+    function whiteSquare(){
+        djSquare.style.backgroundColor = "white"
+    }
 
-function redSquare(){
-    djSquare.style.backgroundColor = "red"
-}
+    function redSquare(){
+        djSquare.style.backgroundColor = "red"
+    }
 
-function yellowSquare(){
-     djSquare.style.backgroundColor = "yellow"
-}
+    function yellowSquare(){
+        djSquare.style.backgroundColor = "yellow"
+    }
 
-function greenSquare(){
-    djSquare.style.backgroundColor = "green"
-}
+    function greenSquare(){
+        djSquare.style.backgroundColor = "green"
+    }
 
-function orangeSquare(){
-    djSquare.style.backgroundColor = "orange"
-}
+    function orangeSquare(){
+        djSquare.style.backgroundColor = "orange"
+    }
 
+window.addEventListener("keydown", type)
+
+    function type(event){
+    if (event.keyCode === 82){
+       redSquare()
+    } else if(event.keyCode === 79){
+        orangeSquare()
+    }else if(event.keyCode === 71){
+        greenSquare()
+    } else if (event.keyCode === 89){
+        yellowSquare()
+    } else if (event.keyCode === 66){
+        blueSquare()
+    } else if (event.keyCode === 87){
+        whiteSquare()
+    }
+}
 
 document.addEventListener("wheel", orangeSquare)
 djSquare.addEventListener("dblclick", greenSquare)
