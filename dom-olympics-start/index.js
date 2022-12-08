546a90ea1c7fde085cf33c4472666f7bf4240d50
michael hosler <mimic.orange@gmail.com>
2022-12-07 18:18:53 -0600

    finished, at least for now

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -1,15 +1,30 @@
-var banner = "This Is The Header For The Dom Olympics!"
 
+
+//HEADER STUFF
+
+var banner = document.createElement("h1")
+var namePart = document.createElement("p")
+var span = document.createElement("span")
+var writeSub = document.getElementById("sub")
 var writeIt = document.getElementById ("header")
+
+
  writeIt.append(banner)
- header.style.color = "blue"
- header.style.fontSize = "50px"
+ writeIt.append(namePart)
+
+banner.textContent = "JavaScript Made This!"
+ banner.style.color = "blue"
+ banner.style.fontSize = "50px"
+
+ namePart.textContent = "Michael H."
+ namePart.style.color = "red"
+ namePart.append(span) 
+
+span.textContent = "Wrote This Script"
+span.style.color = "black"
 
 
-
-
-
-
+//CLEAR BUTTON STUFF
 
 var clrBtn = document.getElementById("clear-button")
 var messages = document.getElementById("text")
@@ -19,3 +34,14 @@ clrBtn.addEventListener("click", blank)
 function blank(){
    messages.remove()
 }
+
+//test[0].textContnnt = "Put string here"
+//CHANGE TEXT STUFF
+
+var changeText = document.getElementsByClassName("message")
+
+changeText[0].textContent = "Hey! How are you?"
+changeText[1].textContent = "I'm doing great! I picked up a new book yesteray and I've spent the whole day in the park reading it. "
+changeText[2].textContent = "Oh? You'll have to tell me all about it later at dinner."
+changeText[3].textContent = "I was going to do that whther you wanted me to or not! (:"
+
