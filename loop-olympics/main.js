467f03fc8dfab90df5f784ebda48ae405e23062c
michael hosler <mimic.orange@gmail.com>
2023-01-23 17:33:38 -0600

    final

---
+++ loop-olympics/main.js
@@ -0,0 +1,50 @@
+const readline = require("readline-sync")
+
+
+const numbers = []
+
+const fruitsOne = ["banana", "orange", "apple", "kiwi"]
+const fruitsTwo = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+const fruitsThree = []
+
+
+//PRINT 0-9
+
+//     for(n = 0; n < 10; n ++){
+//     console.log(n)
+// }
+
+    //PRINT 9-0
+
+    // for (n = 9; n >= 0; n--){
+    //     console.log(n)
+    // }
+
+        //FRUIT LOOPS
+
+        //    for(i = 0; i < fruitsOne.length; i++){
+        //     console.log(fruitsOne[i])
+        //    }
+
+            //PUSH 0-9
+
+                // for(n = 0; n < 10; n++ ){
+                //     numbers.push(n)
+                // }
+                // console.log(numbers)
+
+
+                    //PRINT 0-100 EVENS
+                    
+                        // for(n = 0; n <= 100; n++){
+                        //     if(n % 2 === 0){console.log(n)
+                        //     }
+                        // }
+
+                            //OTHER FRUIT LOOPS
+
+                                // for(i = 0; i < fruitsTwo.length; i++){
+                                //     if(i % 2 === 0){fruitsThree.push(fruitsTwo[i])}
+                                // }
+
+                                // console.log(fruitsThree)
